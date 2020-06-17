61c08b2e500f0c6402ab6a4670ce1b01c9e84350
Phil Wang <lucidrains@gmail.com>
2020-06-17 08:16:32 -0700

    bump package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'byol-pytorch',
   packages = find_packages(),
-  version = '0.0.2',
+  version = '0.0.3',
   license='MIT',
   description = 'Self-supervised contrastive learning made simple',
   author = 'Phil Wang',
