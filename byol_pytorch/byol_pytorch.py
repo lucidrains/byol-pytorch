9b646a23f4bcb562c1447f931c755737c7fcac49
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2020-06-24 12:05:44 +0500

    [#6] make loss positive

--- byol_pytorch/byol_pytorch.py
+++ byol_pytorch/byol_pytorch.py
@@ -36,7 +36,7 @@ def singleton(cache_key):
 def loss_fn(x, y):
     x = F.normalize(x, dim=-1, p=2)
     y = F.normalize(y, dim=-1, p=2)
-    return -2 * (x * y).sum(dim=-1)
+    return 2 - 2 * (x * y).sum(dim=-1)
 
 # augmentation utils
 
