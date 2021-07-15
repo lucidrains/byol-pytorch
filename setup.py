caa65d7c19a80a3611f4049c638ecd8401f8f3c4
Phil Wang <lucidrains@gmail.com>
2021-07-15 10:58:42 -0700

    add more informative error message

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'byol-pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '0.5.6',
+  version = '0.5.7',
   license='MIT',
   description = 'Self-supervised contrastive learning made simple',
   author = 'Phil Wang',
