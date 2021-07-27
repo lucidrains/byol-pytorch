e53affc96c0b36d2b0dddbda1e1a9e8888c0841b
fabio.ferreira@student.kit.edu <fabiof@cs.stanford.edu>
2021-07-27 20:18:34 +0200

    runnable resnet50 on imagenet with logging and torch ddp

--- cluster/metassl.submit_jobs.sh
+++ cluster/metassl.submit_jobs.sh
@@ -27,9 +27,9 @@ echo "create experiment configs"
 
 
 if [ $USER == "frankej" ]; then
-  JOBS=$(/home/frankej/miniconda3/bin/python $WORKFOLDER/metassl/cluster_create_ddp_resnet_base_training.py --project_dir $WORKFOLDER --gpus $GPUS --sess $SESSION 2>&1)
+  JOBS=$(/home/frankej/miniconda3/bin/python $WORKFOLDER/cluster/cluster_create_ddp_resnet_base_training.py --project_dir $WORKFOLDER --gpus $GPUS --sess $SESSION 2>&1)
 elif [ $USER == "ferreira" ]; then
-  JOBS=$(/home/ferreira/.miniconda/envs/metassl/bin/python $WORKFOLDER/metassl/cluster_create_ddp_resnet_base_training.py --project_dir $WORKFOLDER --gpus $GPUS --sess $SESSION 2>&1)
+  JOBS=$(/home/ferreira/.miniconda/envs/metassl/bin/python $WORKFOLDER/cluster/cluster_create_ddp_resnet_base_training.py --project_dir $WORKFOLDER --gpus $GPUS --sess $SESSION 2>&1)
 fi
 
 
