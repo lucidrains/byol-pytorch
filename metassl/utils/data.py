0a37c9c26e6f780bd72ddb0026572562a353fe8b
fabio.ferreira@student.kit.edu <fabiof@cs.stanford.edu>
2021-07-28 17:38:15 +0200

    added ColorJitter transform to ImageNet dataloading

--- metassl/utils/data.py
+++ metassl/utils/data.py
@@ -95,6 +95,11 @@ def get_train_valid_loader(
             [
                 transforms.RandomResizedCrop((224, 224), scale=(0.08, 1.0), interpolation=Image.BICUBIC),
                 transforms.RandomHorizontalFlip(),
+                transforms.ColorJitter(
+                    brightness=0.4,
+                    contrast=0.4,
+                    saturation=0.4,
+                    ),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                 ]
