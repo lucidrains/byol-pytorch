ae87419669dd2687c57f8e53a67c1f20e14375a7
fabio.ferreira@student.kit.edu <fabiof@cs.stanford.edu>
2021-07-28 14:38:08 +0200

    running default config with sbatch

--- cluster/meta_worker_default.sh
+++ cluster/meta_worker_default.sh
@@ -8,5 +8,5 @@ elif [ $USER == "ferreira" ]; then
   source /home/ferreira/.miniconda/bin/activate metassl
 fi
 
-python $PROJECT/cluster/cluster_start_default_config.py --data_dir=$1 --expt_dir=$2 --project_dir=$3 --job_name=$4 --sess=$5
+python $3/cluster/cluster_start_default_config.py --data_dir=$1 --expt_dir=$2 --project_dir=$3 --job_name=$4 --sess=$5
 
