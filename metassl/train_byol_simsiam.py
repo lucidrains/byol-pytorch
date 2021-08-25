c9a8de6304f2836cf06b30736d2372bfe5367024
fabio.ferreira@student.kit.edu <fabiof@cs.stanford.edu>
2021-08-25 16:28:58 +0100

    added knn classification

--- metassl/train_byol_simsiam.py
+++ metassl/train_byol_simsiam.py
@@ -16,25 +16,23 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import numpy as np
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 # original torchvision resnet implementation:
 from torchvision.models import resnet18, resnet50  # torchvision (for ImageNet)
 
-from metassl.models.resnet import ResNet
 from metassl.utils.data import get_test_loader, get_train_valid_loader
 from metassl.utils.my_optimizer import MyOptimizer
 from metassl.utils.summary import SummaryDict
 from metassl.utils.supporter import Supporter
 from metassl.utils.torch_utils import count_parameters
+from metassl.utils.torch_utils import knn_predict
 
 # custom implementation; faster but smaller complexity through fewer conv layers and smaller fc layer (for CIFAR10/100):
 # from metassl.models.ResNet_CIFAR_small import resnet20, resnet56
 
 # custom implementation; close to the original implementation with 3x3 instead of 7x7 conv1 and stride 1 (for CIFAR10/100):
 # from metassl.models.ResNet_CIFAR import resnet18, resnet50
-
-print("CUDA", torch.cuda.is_available())
-
 
 def train_model(config, logger, checkpoint, local_rank):
     logger.print_config(config)
@@ -58,6 +56,7 @@ def train_model(config, logger, checkpoint, local_rank):
         batch_size=config.train.batch_size,
         random_seed=config.data.seed,
         dataset_name=config.data.dataset,
+        shuffle=True,
         num_workers=50,
         pin_memory=False,
         download=True,
@@ -75,9 +74,10 @@ def train_model(config, logger, checkpoint, local_rank):
         )
     
     out_size = len(train_loader.dataset.classes)
-    
-    logger("device", device)
-    logger("out_size", out_size)
+
+    if not distributed or distributed and local_rank == 0:
+        logger("device", device)
+        logger("out_size", out_size)
     
     if config.model.model_type == "resnet18":
         model = resnet18(pretrained=False).to(device)
@@ -90,7 +90,7 @@ def train_model(config, logger, checkpoint, local_rank):
         # model = resnet56(num_classes=out_size).to(device)
     else:
         raise ValueError(f'Model not valid: {config.model.model_type}')
-
+    
     if distributed:
         model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     
@@ -102,30 +102,32 @@ def train_model(config, logger, checkpoint, local_rank):
         use_momentum=False  # turn off momentum in the target encoder
         )
     
-    
     model = DDP(
         model.to(device),
         device_ids=[local_rank],
         find_unused_parameters=True,
         )
     
-    logger.log("model_parameters", count_parameters(model.parameters()))
+    if not distributed or distributed and local_rank == 0:
+        logger.log("model_parameters", count_parameters(model.parameters()))
+        logger("START initial validation")
     
     summary_dict = SummaryDict()
     
-    logger("START initial validation")
-    
-    valid_loss, accuracy = test(config, model, device, train_loader, valid_loader, local_rank)
-    summary_dict["step"] = 0
-    summary_dict["valid_loss"] = valid_loss
-    summary_dict["valid_accuracy"] = accuracy
-    summary_dict["train_loss"] = 0
-    summary_dict["learning_rate"] = 0
-    
     iter_per_epoch = len(train_loader)
     max_steps = config.train.epochs * iter_per_epoch
-    logger("iter_per_epoch", iter_per_epoch)
-    logger("max_steps", max_steps)
+    
+    # valid_loss, accuracy = test_linear_classification(config, model, device, train_loader, valid_loader, local_rank)
+    if not distributed or distributed and local_rank == 0:
+        accuracy = test_knn(model, device, train_loader, valid_loader, out_size)
+        logger("knn test acc: ", accuracy)
+        summary_dict["step"] = 0
+        summary_dict["valid_accuracy"] = accuracy
+        summary_dict["train_loss"] = 0
+        summary_dict["learning_rate"] = 0
+    
+        logger("iter_per_epoch", iter_per_epoch)
+        logger("max_steps", max_steps)
     
     optimizer = MyOptimizer(0, model.parameters(), max_steps, iter_per_epoch, **config.optim.get_dict)
     
@@ -135,9 +137,10 @@ def train_model(config, logger, checkpoint, local_rank):
     
     for epoch in range(epoch_resume, config.train.epochs):
         train_loader.sampler.set_epoch(epoch)
-        
-        logger(f"START epoch {epoch}")
-        logger.start_timer("train")
+
+        if not distributed or distributed and local_rank == 0:
+            logger(f"START epoch {epoch}")
+            logger.start_timer("train")
         
         model.train()
         
@@ -151,10 +154,17 @@ def train_model(config, logger, checkpoint, local_rank):
             optimizer.step()
             train_loss.append(loss.detach().cpu().numpy())
             
-            if step % config.train.eval_freq == 0:
+            if not distributed and step % config.train.eval_freq == 0:
                 print(f"Epoch {epoch:{5}} / {config.train.epochs:{5}}, Loss {loss.item():.5f}")
+            elif distributed and step % config.train.eval_freq == 0:
+                loss = loss.clone().detach()
+                dist.all_reduce(loss, op=dist.ReduceOp.SUM)
+                loss /= dist.get_world_size()
+                if dist.get_rank() == 0:
+                    print(f"Epoch {epoch:{5}} / {config.train.epochs:{5}}, Loss {loss.item():.5f}")
         
-        if config.expt.save_model and epoch % config.expt.save_model_freq == 0:
+        if not distributed and config.expt.save_model and epoch % config.expt.save_model_freq == 0 or \
+            (distributed and config.expt.save_model and epoch % config.expt.save_model_freq == 0 and local_rank == 0):
             checkpoint.save_training(
                 model_state_dict=model.state_dict(),
                 optimizer_state_dict=optimizer.get_state_dict(),
@@ -165,33 +175,104 @@ def train_model(config, logger, checkpoint, local_rank):
         
         logger.timer("train", epoch)
         
-        valid_loss, accuracy = test(model, device, valid_loader)
-        summary_dict["step"] = epoch + 1
-        summary_dict["train_loss"] = np.mean(train_loss)
-        summary_dict["valid_loss"] = valid_loss
-        summary_dict["valid_accuracy"] = accuracy
-        summary_dict["learning_rate"] = optimizer._rate
-        logger("train_loss", np.mean(train_loss), epoch)
-        logger("valid_loss", valid_loss, epoch)
-        logger("valid_accuracy", accuracy, epoch)
-        logger("learning_rate", optimizer._rate, epoch)
-        
-        summary_dict.save(checkpoint.dir / "summary_dict.npy")
+        # valid_loss, accuracy = test_linear_classification(model, device, valid_loader)
+        if not distributed or distributed and local_rank == 0:
+            accuracy = test_knn(model, device, train_loader, valid_loader, out_size)
+            summary_dict["step"] = epoch + 1
+            summary_dict["train_loss"] = np.mean(train_loss)
+            # summary_dict["valid_loss"] = valid_loss
+            summary_dict["valid_accuracy"] = accuracy
+            summary_dict["learning_rate"] = optimizer._rate
+            logger("train_loss", np.mean(train_loss), epoch)
+            # logger("valid_loss", valid_loss, epoch)
+            logger("valid_accuracy", accuracy, epoch)
+            logger("learning_rate", optimizer._rate, epoch)
+            
+            summary_dict.save(checkpoint.dir / "summary_dict.npy")
     
     # TEST FINAL MODEL
-    # test_loss, test_accuracy = test(model, device, test_loader)
-    # summary_dict["test_loss"] = test_loss
-    # summary_dict["test_accuracy"] = test_accuracy
-    # logger("test_loss", test_loss, epoch)
-    # logger("test_accuracy", test_accuracy, epoch)
+    # if not distributed or distributed and local_rank == 0:
+    #     test_loss, test_accuracy = test(model, device, test_loader)
+    #     summary_dict["test_loss"] = test_loss
+    #     summary_dict["test_accuracy"] = test_accuracy
+    #     logger("test_loss", test_loss, epoch)
+    #     logger("test_accuracy", test_accuracy, epoch)
+    #     summary_dict.save(checkpoint.dir / "summary_dict.npy")
+
+
+def test_knn(
+    byol_model: BYOL,
+    device,
+    train_loader,
+    test_loader,
+    n_classes,
+    knn_n_neighbours=32,
+    knn_reweighting_factor=0.1,
+    knn_n_batches=1,
+    knn_normalize_features=True,
+    ):
+    byol_model.eval()
     
-    summary_dict.save(checkpoint.dir / "summary_dict.npy")
+    # get train features for fitting knn
+    train_features = []
+    train_labels = []
+    for step, (data, target) in enumerate(train_loader):
+        data = data.to(device)
+        _, embedding = byol_model(data, return_embedding=True)
+        if knn_normalize_features:
+            embedding = F.normalize(embedding, dim=1)
+        train_features.append(embedding)
+        
+        target = target.to(device)
+        train_labels.append(target)
+        
+        if step == knn_n_batches:
+            break
+    
+    train_features = torch.cat(train_features, dim=0).t().contiguous()
+    print("train_features", train_features.size())
+    train_labels = torch.cat(train_labels, dim=0).t().contiguous()
+    
+    # knn predict
+    test_features = []
+    test_labels = []
+    for step, (data, target) in enumerate(test_loader):
+        data = data.to(device)
+        _, embedding = byol_model(data, return_embedding=True)
+        if knn_normalize_features:
+            embedding = F.normalize(embedding, dim=1)
+        test_features.append(embedding)
+        
+        target = target.to(device)
+        test_labels.append(target)
+        
+        if step == 0:
+            break
+    
+    test_features = torch.cat(test_features, dim=0)
+    test_labels = torch.cat(test_labels, dim=0)
+    
+    # print("test_features shape", test_features.size())
+    # print("train_features shape", train_features.size())
+    # print("train_labels shape", train_labels.size())
+    pred_labels = knn_predict(test_features, train_features, train_labels, n_classes, knn_n_neighbours, knn_reweighting_factor)
+    pred_labels = pred_labels.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    # print("pred_labels size", pred_labels.size())
+    # print("test_labels size", test_labels.size())
+    # print("test_labels view as", test_labels.view_as(pred_labels).size())
+    correct = pred_labels.eq(test_labels.view_as(pred_labels)).sum().item()
+    
+    accuracy = 100. * correct / test_labels.size(0)
+    print("number of knn test samples correct", correct)
+    print("knn test accuracy", accuracy)
+    return accuracy
 
 
-def test(config, byol_model: BYOL, device, train_loader, test_loader, local_rank):
+def test_linear(config, byol_model: BYOL, device, train_loader, test_loader, local_rank):
+    # todo
     byol_model.eval()
     model = byol_model.module.net
-    model.fc = DDP(model.fc, device_ids=[local_rank], find_unused_parameters=True,)
+    model.fc = DDP(model.fc, device_ids=[local_rank], find_unused_parameters=True, )
     require_grads = []
     for p in model.parameters():
         require_grads.append(p.requires_grad)
@@ -199,37 +280,33 @@ def test(config, byol_model: BYOL, device, train_loader, test_loader, local_rank
     
     for p in model.fc.parameters():
         p.requires_grad_(True)
-
+    
     iter_per_epoch = len(train_loader)
     max_steps = config.finetuning.epochs * iter_per_epoch
     finetuning_optimizer = MyOptimizer(0, model.fc.parameters(), max_steps, iter_per_epoch, **config.finetuning.optim.dict)
-
+    
     criterion = nn.CrossEntropyLoss().to(device)
-
+    
     for epoch in range(1, config.finetuning.epochs):
         train_loader.sampler.set_epoch(epoch)
-    
-        #logger(f"START epoch {epoch}")
-        #logger.start_timer("train")
-    
-    
+        
+        # logger(f"START epoch {epoch}")
+        # logger.start_timer("train")
+        
         train_loss = []
         for step, (data, target) in enumerate(train_loader):
             data = data.to(device)
             target = target.to(device)
             y = model(data)
-            l = criterion(y,target)
+            l = criterion(y, target)
             finetuning_optimizer.zero_grad()
             l.backward()
             finetuning_optimizer.step()
             train_loss.append(l.item())
-            print('finetune train loss',l)
-            
+            print('finetune train loss', l)
+    
     for p, rg in zip(model.parameters(), require_grads):
         p.requires_grad_(rg)
-    
-    
-    
     
     test_loss = 0
     correct = 0
--- metassl/utils/torch_utils.py
+++ metassl/utils/torch_utils.py
@@ -1,9 +1,8 @@
 import math
-from typing import TypeVar, Optional, Iterator, Sequence
+from typing import TypeVar, Optional, Iterator
 
 import torch
 import torch.distributed as dist
-
 from torch.utils.data import Sampler
 
 
@@ -133,3 +132,42 @@ class DistributedSampler(Sampler[T_co]):
             epoch (int): Epoch number.
         """
         self.epoch = epoch
+
+
+# code for kNN prediction from here:
+# https://colab.research.google.com/github/facebookresearch/moco/blob/colab-notebook/colab/moco_cifar10_demo.ipynb
+def knn_predict(
+    feature, feature_bank, feature_labels, classes: int, knn_k: int, knn_t: float
+    ):
+    """Helper method to run kNN predictions on features based on a feature bank
+    Args:
+        feature: Tensor of shape [N, D] consisting of N D-dimensional features
+        feature_bank: Tensor of a database of features used for kNN
+        feature_labels: Labels for the features in our feature_bank
+        classes: Number of classes (e.g. 10 for CIFAR-10)
+        knn_k: Number of k neighbors used for kNN
+        knn_t:
+    """
+    # compute cos similarity between each feature vector and feature bank ---> [B, N]
+    sim_matrix = torch.mm(feature, feature_bank)
+    # [B, K]
+    sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
+    # [B, K]
+    sim_labels = torch.gather(
+        feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices
+        )
+    # we do a reweighting of the similarities
+    sim_weight = (sim_weight / knn_t).exp()
+    # counts for each class
+    one_hot_label = torch.zeros(
+        feature.size(0) * knn_k, classes, device=sim_labels.device
+        )
+    # [B*K, C]
+    one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
+    # weighted score ---> [B, C]
+    pred_scores = torch.sum(
+        one_hot_label.view(feature.size(0), -1, classes) * sim_weight.unsqueeze(dim=-1),
+        dim=1,
+        )
+    pred_labels = pred_scores.argsort(dim=-1, descending=True)
+    return pred_labels
