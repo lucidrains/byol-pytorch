82ae12957a3c15e1a315f3879da8e286f4690750
ferreirafabio <ferreirafabio@users.noreply.github.com>
2021-12-19 14:45:25 +0100

    removing unused scripts

---
+++ cluster/submit_alternating-parameterized-aug-ftlr-100-ptlr-5e-2-bs-256-epochs-200-warmup-0.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH -p alldlc_gpu-rtx2080
+#SBATCH --gres=gpu:8
+#SBATCH --job-name=alternating-parameterized-aug-ftlr-100-ptlr-5e-2-bs-256-epochs-200-warmup-0
+#SBATCH -o /home/ferreira/workspace/experiments/metassl/logs/%x.%N.%A.%a.out
+#SBATCH --array=0-10%1
+
+TRAIN_EPOCHS=200
+FINETUNING_EPOCHS=200
+WARMUP=0
+EXPT_NAME="alternating-parameterized-aug-ftlr-100-ptlr-5e-2-bs-256-epochs-200-warmup-0"
+
+echo "TRAIN EPOCHS $TRAIN_EPOCHS"
+echo "FINETUNING EPOCHS $FINETUNING_EPOCHS"
+echo "EXPT NAME $EXPT_NAME"
+echo "WARMUP $WARMUP"
+
+#PARTITION="ml_gpu-rtx2080"
+#PARTITION="bosch_gpu-rtx2080"
+#PARTITION="gpu_tesla-P100"
+#PARTITION="meta_gpu-ti"
+#PARTITION="alldlc_gpu-rtx2080"
+#PARTITION="mldlc_gpu-rtx2080"
+#PARTITION="testdlc_gpu-rtx2080"
+
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-384
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
+
+#export NCCL_DEBUG=INFO
+
+
+WORKFOLDER="/home/$USER/workspace/metassl"
+echo $WORKFOLDER
+export PYTHONPATH=$PYTHONPATH:$WORKFOLDER
+
+source /home/ferreira/.miniconda/bin/activate metassl
+
+echo "submitted job $EXPT_NAME"
+echo "logfile at $LOG_FILE"
+echo "error file at $ERR_FILE"
+
+srun $WORKFOLDER/cluster/train_alternating_simsiam_warmup_parameterized_aug.sh $EXPT_NAME $TRAIN_EPOCHS $FINETUNING_EPOCHS $WARMUP
