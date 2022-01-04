1bb306f854e5a1d4d08c118ae6c3be650ae8564c
wagnerd <wagnerd@informatik.uni-freiburg.de>
2022-01-04 23:06:09 +0100

    Update submission script (ImageNet)

--- cluster/submit_alternating_simsiam_imagenet_workspace.sh
+++ cluster/submit_alternating_simsiam_imagenet_workspace.sh
@@ -2,15 +2,15 @@
 #SBATCH -q dlc-wagnerd # partition (queue)
 #SBATCH -p mlhiwidlc_gpu-rtx2080
 #SBATCH -t 1-00:00 # time (D-HH:MM)
-#SBATCH --gres=gpu:1
-#SBATCH -J mssl-d-cifar10-simsiam # sets the job name. If not specified, the file name will be used as job name
+#SBATCH --gres=gpu:8
+#SBATCH -J mssl-d-imagenet-simsiam # sets the job name. If not specified, the file name will be used as job name
 # Print some information about the job to STDOUT
 echo "Workingdir: $PWD";
 echo "Started at $(date)";
 echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
 
 # Job to perform
-python -m metassl.train_alternating_simsiam --expt.expt_name $EXPERIMENT_NAME --config "metassl/default_metassl_config_imagenet.yaml" --expt.is_non_grad_based --use_fixed_args --finetuning.valid_size 0.1 --train.epochs 90 --finetuning.epochs 90 --expt.warmup_epochs 0
+python -m metassl.train_alternating_simsiam --expt.expt_name $EXPERIMENT_NAME --config "metassl/default_metassl_config_imagenet.yaml" --expt.is_non_grad_based --use_fixed_args --finetuning.valid_size 0.1 --train.epochs 90 --finetuning.epochs 90 --expt.warmup_epochs 0 --expt.workers 2
 
 # Print some Information about the end-time to STDOUT
 echo "DONE";
